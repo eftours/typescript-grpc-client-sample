387aabe1ebb31c946e4c8d13a5cb55e71fbe2b64
Justin Capalbo <capalbo.justin@gmail.com>
2022-02-16 13:00:10 -0500

    Addendum regarding promises

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,30 @@ import { credentials, loadPackageDefinition } from "@grpc/grpc-js";
 import { loadSync } from "@grpc/proto-loader";
 import { ProtoGrpcType } from "../proto/greeter_service";
 import { GreetRequest } from "../proto/greeter_service/GreetRequest";
+import { GreetResponse } from "../proto/greeter_service/GreetResponse";
 
 const proto = loadPackageDefinition(loadSync("./greeter_service.proto")) as unknown as ProtoGrpcType;
 const greeterClient = new proto.greeter_service.GreeterService("0.0.0.0:50051", credentials.createInsecure());
 
-const greetRequest: GreetRequest = {
-	name: "Justin",
-	location: "MA",
-};
+const start = async () => {
+	const greetRequest: GreetRequest = {
+		name: "Justin",
+		location: "MA",
+	};
+	const response = await greet(greetRequest);
+	console.log(response);
+}
 
-greeterClient.Greet(greetRequest, (err, response) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(response.greeting);
-	}
-});
+const greet = (request: GreetRequest): Promise<GreetResponse> => {
+	return new Promise((resolve, reject) => {
+		greeterClient.Greet(request, (err, response) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+}
+
+start();
